6043d6dfd2b6ce27058bd8718d00236ef48ebca0
Marcus <marcusvinicius.767.mv@gmail.com>
2024-09-04 16:55:29 -0300

    ajustado organizacao de pastas

---
+++ src/screens/Home/styles.ts
@@ -0,0 +1,15 @@
+import { Text, View, StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#131016",
+    padding: 24,
+  },
+  eventName: {
+    color: "#FDFCFE",
+    fontSize: 24,
+    fontWeight: "bold",
+    marginTop: 48,
+  },
+});
