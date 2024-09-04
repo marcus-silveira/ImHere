ed098b66f3fb82316702a4fb79a76912abf61ae4
Marcus Silveira <marcusvinicius.767.mv@gmail.com>
2024-09-04 20:52:21 -0300

    Criado botao de add e alterado style

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,16 +1,25 @@
-import { Text, View, TextInput } from "react-native";
+import { Text, View, TextInput, TouchableOpacity } from "react-native";
 import { styles } from "./styles";
 
 export function Home() {
+  function handleParticipantAdd() {
+    console.log("BOTAO CLICADO");
+  }
   return (
     <View style={styles.container}>
       <Text style={styles.eventName}>Nome do Evento</Text>
 
-      <TextInput
-        style={styles.input}
-        placeholder="Nome do Participante"
-        placeholderTextColor="#6B6B6B"
-      />
+      <View style={styles.form}>
+        <TextInput
+          style={styles.input}
+          placeholder="Nome do Participante"
+          placeholderTextColor="#6B6B6B"
+        />
+
+        <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
+          <Text style={styles.ButtonText}>+</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
